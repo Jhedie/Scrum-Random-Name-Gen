c8436d355971d0c1aff96939dd96c2ff20e412d8
JedidiahAwuku <jedidiah.awuku@sap.com>
2022-11-01 20:09:47 +0000

    Add Winwheel JS asset

--- src/app/spinner-section/spinner-section.component.ts
+++ src/app/spinner-section/spinner-section.component.ts
@@ -1,4 +1,3 @@
-import { animation } from '@angular/animations';
 import {
   AfterViewInit,
   Component,
@@ -7,70 +6,25 @@ import {
   Renderer2,
   ViewChild,
 } from '@angular/core';
+
+declare let Winwheel: any;
 @Component({
   selector: 'app-spinner-section',
   templateUrl: './spinner-section.component.html',
   styleUrls: ['./spinner-section.component.scss'],
 })
 export class SpinnerSectionComponent implements OnInit, AfterViewInit {
-  SLOTS_PER_REEL = 12; //ratio = 1:12.5
-  REEL_RADIUS = 150;
-  @ViewChild('mainRing') mainRing!: ElementRef;
+  theWheel: any;
+  nameList: [] = JSON.parse(localStorage.getItem('storedCards') || '');
+  numSegments: number = this.nameList.length;
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
   ngAfterViewInit(): void {
-    this.createSlots(this.mainRing);
+    this.theWheel = new Winwheel({ numSegments: this.numSegments });
   }
-  ngOnInit(): void {}
-
-  createSlots(ring: ElementRef) {
-    let slotAngle = 360 / this.SLOTS_PER_REEL;
-
-    let seed = this.getSeed();
-
-    for (let i: number = 0; i < this.SLOTS_PER_REEL; i++) {
-      let slot = this.renderer.createElement('div');
-      this.renderer.addClass(slot, 'slot');
-
-      // compute and assign the transform for this slot
-      let transform: string =
-        'rotateX(' +
-        slotAngle * i +
-        'deg) translateZ(' +
-        this.REEL_RADIUS +
-        'px)';
-
-      this.renderer.setStyle(slot, 'transform', transform);
-
-      // setup the number to show inside the slot
-      // the position is randomized to
-      let slotContent = this.renderer.createText(
-        `${(seed + i) % this.SLOTS_PER_REEL}`
-      );
-      let slotContentParagraph = this.renderer.createElement('p');
-      this.renderer.appendChild(slotContentParagraph, slotContent);
-      this.renderer.appendChild(slot, slotContentParagraph);
-      // add the poster to the row
-      this.renderer.appendChild(ring.nativeElement, slot);
-    }
+  ngOnInit(): void {
+    console.log(this.nameList, this.numSegments);
   }
 
-  getSeed(): number {
-    // generate random number smaller than 13 then floor it to settle between 0 and 12 inclusive
-    return Math.floor(Math.random() * this.SLOTS_PER_REEL);
-  }
-
-  spin(timer: number) {
-    this.renderer.setStyle(
-      this.mainRing.nativeElement,
-      'animation',
-      `back-spin 1s, spin ${timer * 0.5}s'`
-    );
-    this.renderer.addClass(this.mainRing.nativeElement, 'spin');
-  }
-
-  rotateFunction(): void {
-    let timer: number = 2;
-    this.spin(timer);
-  }
+  rotateFunction(): void {}
 }
