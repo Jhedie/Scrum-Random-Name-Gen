a022c6862f2643cc3f559c2c9ec39fa575ba3c64
JedidiahAwuku <jedidiah.awuku@sap.com>
2022-11-15 13:42:19 +0000

    feat: Create wheel service and refactor spinner-section component.

--- src/app/Services/WheelService/wheel.service.ts
+++ src/app/Services/WheelService/wheel.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
-
+import { Card } from 'src/app/Models/card.model';
+import { Segment } from 'src/app/Models/Segment.model';
+declare let Winwheel: any;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WheelService {
+  theWheel: any;
+  segments: Segment[] = [];
+  nameList!: Card[];
 
-  constructor() { }
+  createWheel(names: Card[]): void {
+    this.nameList = names;
+    this.createSegments();
+    this.theWheel = new Winwheel({
+      numSegments: this.getNumberOfSegments(),
+      segments: this.segments,
+      fillStyle: '#FFFFFF',
+      outerRadius: 225,
+      rotationAngle: -30,
+      animation: {
+        type: 'spinToStop', // Type of animation.
+        duration: 5, // How long the animation is to take in seconds.
+        spins: 10, // The number of complete 360 degree rotations the wheel is to do.
+        callbackFinished: 'getVolunteer(this)',
+        callbackAfter: 'finished(this)',
+      },
+    });
+
+    this.theWheel.drawInitialTriangle();
+  }
+
+  createSegments() {
+    const colourPallete: string[] = [
+      '#8E3B46',
+      '#9E7676',
+      '#EDDBC0',
+      '#8BBCCC',
+      '#DFD3C3',
+      '#AEBDCA',
+      '#90B77D',
+      '#EBC7E8',
+    ];
+    this.segments = this.nameList.map((card, index) => ({
+      text: card.name,
+      fillStyle: colourPallete[index],
+    }));
+  }
+
+  getNumberOfSegments(): number {
+    return this.segments.length;
+  }
+
+  resetWheel() {
+    this.theWheel.stopAnimation(false);
+    this.theWheel.rotationAngle = 0; // Re-set the wheel angle to 0 degrees.
+    this.createSegments();
+  }
+
+  spinWheel() {
+    this.theWheel.startAnimation();
+  }
 }
--- src/app/spinner-section/spinner-section.component.ts
+++ src/app/spinner-section/spinner-section.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 import { Card } from '../Models/card.model';
 import { Segment } from '../Models/Segment.model';
 import { NameListingService } from '../Services/nameListingService/name-listing.service';
+import { WheelService } from '../Services/WheelService/wheel.service';
 
 declare let Winwheel: any;
 @Component({
@@ -16,63 +17,26 @@ export class SpinnerSectionComponent implements OnInit {
   numSegments!: number;
   segments: Segment[] = [];
 
-  constructor(private nameListingService: NameListingService) {}
-
-  initialiseWheel(): void {
-    this.theWheel = new Winwheel({
-      numSegments: this.numSegments,
-      segments: this.segments,
-      fillStyle: '#FFFFFF',
-      outerRadius: 225,
-      rotationAngle: -30,
-      animation: {
-        type: 'spinToStop', // Type of animation.
-        duration: 5, // How long the animation is to take in seconds.
-        spins: 10, // The number of complete 360 degree rotations the wheel is to do.
-        callbackFinished: 'getVolunteer(this)',
-        callbackAfter: 'finished(this)',
-      },
-    });
-    this.theWheel.drawInitialTriangle();
-  }
+  constructor(
+    private nameListingService: NameListingService,
+    private wheelService: WheelService
+  ) {}
 
   ngOnInit(): void {
     this.nameListingService
       .getAllNames()
       .pipe(
         map((cards) => {
-          this.nameList = cards;
-          this.numSegments = this.nameList.length;
-          this.createSegments();
-          this.initialiseWheel();
+          this.wheelService.createWheel(cards);
         })
       )
       .subscribe();
   }
 
   rotateFunction(): void {
-    this.theWheel.startAnimation();
+    this.wheelService.spinWheel();
   }
   resetSpinner() {
-    this.theWheel.stopAnimation(false);
-    this.theWheel.rotationAngle = 0; // Re-set the wheel angle to 0 degrees.
-    this.initialiseWheel();
-  }
-
-  createSegments() {
-    const colourPallete: string[] = [
-      '#8E3B46',
-      '#9E7676',
-      '#EDDBC0',
-      '#8BBCCC',
-      '#DFD3C3',
-      '#AEBDCA',
-      '#90B77D',
-      '#EBC7E8',
-    ];
-    this.segments = this.nameList.map((card, index) => ({
-      text: card.name,
-      fillStyle: colourPallete[index],
-    }));
+    this.wheelService.resetWheel();
   }
 }
