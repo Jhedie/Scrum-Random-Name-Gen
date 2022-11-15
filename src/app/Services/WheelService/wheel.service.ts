a30f0b8c5226820b37ad7ddd77b211abe74850b9
JedidiahAwuku <jedidiah.awuku@sap.com>
2022-11-15 16:47:38 +0000

    feat: Modify animation to improve randomness in wheel spin

--- src/app/Services/WheelService/wheel.service.ts
+++ src/app/Services/WheelService/wheel.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Card } from 'src/app/Models/card.model';
 import { Segment } from 'src/app/Models/Segment.model';
+
 declare let Winwheel: any;
+declare let Animation: any;
 @Injectable({
   providedIn: 'root',
 })
@@ -19,28 +21,45 @@ export class WheelService {
       fillStyle: '#FFFFFF',
       outerRadius: 225,
       rotationAngle: -30,
-      animation: {
-        type: 'spinToStop', // Type of animation.
-        duration: 5, // How long the animation is to take in seconds.
-        spins: 10, // The number of complete 360 degree rotations the wheel is to do.
-        callbackFinished: 'getVolunteer(this)',
-        callbackAfter: 'finished(this)',
-      },
+      animation: this.getWheelAnimation(),
     });
 
     this.theWheel.drawInitialTriangle();
   }
 
-  createSegments() {
+  getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getWheelAnimation(): Animation {
+    const randomDuration = this.getRandomInt(5, 10);
+    const randomSpinNumber = this.getRandomInt(5, 10);
+    const randomDirection = !!this.getRandomInt(0, 1)
+      ? 'clockwise'
+      : 'anti-clockwise';
+    const animation = new Animation({
+      type: 'spinToStop', // Type of animation.
+      duration: randomDuration, // How long the animation is to take in seconds.
+      spins: randomSpinNumber, // The number of complete 360 degree rotations the wheel is to do.
+      callbackFinished: 'getVolunteer(this)',
+      callbackAfter: 'finished(this)',
+      direction: randomDirection, // clockwise or anti-clockwise.
+    });
+    return animation;
+  }
+
+  createSegments(): void {
     const colourPallete: string[] = [
-      '#8E3B46',
-      '#9E7676',
-      '#EDDBC0',
-      '#8BBCCC',
-      '#DFD3C3',
-      '#AEBDCA',
-      '#90B77D',
-      '#EBC7E8',
+      '#66c2a5',
+      '#f46d43',
+      '#fdae61',
+      '#fee08b',
+      '#e6f598',
+      '#abdda4',
+      '#3288bd',
+      '#d53e4f',
     ];
     this.segments = this.nameList.map((card, index) => ({
       text: card.name,
@@ -52,13 +71,13 @@ export class WheelService {
     return this.segments.length;
   }
 
-  resetWheel() {
+  resetWheel(): void {
     this.theWheel.stopAnimation(false);
     this.theWheel.rotationAngle = 0; // Re-set the wheel angle to 0 degrees.
-    this.createSegments();
+    this.createWheel(this.nameList);
   }
 
-  spinWheel() {
+  spinWheel(): void {
     this.theWheel.startAnimation();
   }
 }
